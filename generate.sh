24554d3f322f41342e122661168cae54cf8a5bdd
Paul Ewing <pcewing00@gmail.com>
2019-10-13 18:10:05 -0700

    Progress

--- generate.sh
+++ generate.sh
@@ -14,7 +14,7 @@ svg_opener="inkscape"
 options="--visual-switch-cutouts --keycap-overlays --keycap-legends"
 
 dotnet "$kbutil_dll" gen-svg $options "$input" "$output"
-#dotnet "$kbutil_dll" gen-key-bearings "$input" "./keys.json" --debug-svg="./temp.svg"
+dotnet "$kbutil_dll" gen-key-bearings "$input" "./keys.json" --debug-svg="./temp.svg"
 
 "$svg_opener" "$output/bloomer_Switch.svg" 
 
