22020b84118ae4e86d0504359f93f41d57f902c5
Paul Ewing <pcewing00@gmail.com>
2021-04-17 00:17:43 -0700

    Updates

--- move_clusters.py
+++ move_clusters.py
@@ -2,7 +2,33 @@
 
 import math
 
-def main():
+def baz():
+    y0 = 507.423
+    y1 = 449.575
+
+def foo():
+    # y = ~527.7
+
+    (x0, y0) = (38.011, 528.271)
+
+    x1 = 28.100
+
+    dx = x0 - x1
+
+    dy = dx * math.tan(math.radians(10))
+    dy = round(dy, 3)
+    print(dy)
+
+    y1 = round(528.271 - dy, 3)
+    print(y1)
+
+    print(round(529.050 - y1, 3))
+
+    # Solve for y1
+
+    # y = mx + b
+
+def bar():
     theta = math.radians(10.0)
     magnitude = 11.0
 
@@ -17,7 +43,7 @@ def main():
 
     print('(x1, y1) = ({}, {})'.format(x1, y1))
 
-    (x2, y2) = (40.585, 413.335)
+    (x2, y2) = (45.602, 412.274)
 
     print('(x2, y2) = ({}, {})'.format(x2, y2))
 
@@ -27,6 +53,11 @@ def main():
     )
 
     print('(dx, dy) = ({}, {})'.format(dx, dy))
+
+
+def main():
+    #bar()
+    foo()
 
     return 0
 
